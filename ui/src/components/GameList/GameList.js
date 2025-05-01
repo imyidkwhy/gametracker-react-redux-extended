cec2f4d6ace1fcfa999ee325648a869f31fce4b3
imyidkwhy <imyidkwhy@gmail.com>
2025-05-01 19:52:06 +0300

    init

--- ui/src/components/GameList/GameList.js
+++ ui/src/components/GameList/GameList.js
@@ -24,13 +24,24 @@ const GameList = () =>{
         const matchesTitle = game.title
             .toLowerCase()
             .includes(titleFilter.toLowerCase());
-        const matchesPrice = game.price
-            .toLowerCase()
-            .includes(priceFilter.toLowerCase());
+const gamePriceString = String(game.price);
+  const matchesPrice = gamePriceString
+    .includes(priceFilter)
         const matchesFavorite = onlyFavoriteFilter ? game.isFavorite : true
         return matchesTitle && matchesPrice && matchesFavorite
     });
-
+    const highlightMatch = (text, filter ) =>{
+        if(!filter) return text
+        const regex = new RegExp(`(${filter})`, 'gi')
+        return text.split(regex).map((substring, i) =>{
+            if(substring.toLowerCase() === filter.toLowerCase()){
+                return(
+                    <span key={i} className="highlight">{substring}</span>
+                )
+            }
+            return substring
+        })
+    }
     return (
         <div className="app-block game-list">
         <h2>Game List </h2>
@@ -49,7 +60,7 @@ const GameList = () =>{
                  
                 {filteredGames.map((game, i) => (
                     <li key={game.id}> 
-                        <div className='game-info'> {++i}. {game.title}  <strong>{game.price}$ </strong></div>
+                        <div className='game-info'> {++i}. {highlightMatch(game.title, titleFilter)} <strong> {highlightMatch(game.price, priceFilter)}<span>$</span> </strong></div>
                         <span onClick={() => handleAddFavorite(game.id)}>
                         {
                             game.isFavorite ? (
